6ffe17af229bfb3cff88172acbe0f90ded4ce588
DESKTOP-4GE1PFI\zadon <zadonskiyilya@gmail.com>
2022-09-19 23:14:19 +0700

    Переключатель сцен

--- Assets/Scripts/Editor/SceneHelper.cs
+++ Assets/Scripts/Editor/SceneHelper.cs
@@ -1,15 +1,21 @@
+using System.Windows.Forms;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.SceneManagement;
 
 public class SceneHelper : EditorWindow
 {
+    private const string scenesPath = "Assets/Scenes/";
+    private const string sceneType  = ".unity";
+
     string myString = "Hello World";
     bool groupEnabled;
     bool myBool = true;
     float myFloat = 1.23f;
-    
     // Add menu item named "My Window" to the Window menu
-    [MenuItem("Window/My Window")]
+    [UnityEditor.MenuItem("Window/My Window")]
     public static void ShowWindow()
     {
         //Show existing window instance. If one doesn't exist, make one.
@@ -18,12 +24,23 @@ public class SceneHelper : EditorWindow
     
     void OnGUI()
     {
-        GUILayout.Label ("Base Settings", EditorStyles.boldLabel);
-        myString = EditorGUILayout.TextField ("Text Field", myString);
+        ButtonChangeScene("Load Menu", "Menu");
+        ButtonChangeScene("Load Level 1", "Level_1");
         
+        /*GUILayout.Label ("Base Settings", EditorStyles.boldLabel);
+        myString = EditorGUILayout.TextField ("Text Field", myString);
+
         groupEnabled = EditorGUILayout.BeginToggleGroup ("Optional Settings", groupEnabled);
             myBool = EditorGUILayout.Toggle ("Toggle", myBool);
             myFloat = EditorGUILayout.Slider ("Slider", myFloat, -3, 3);
-        EditorGUILayout.EndToggleGroup ();
+        EditorGUILayout.EndToggleGroup ();*/
+    }
+
+    private void ButtonChangeScene(string buttonName, string sceneName)
+    {
+        if (GUILayout.Button(buttonName))
+        {
+            EditorSceneManager.OpenScene(scenesPath + sceneName + sceneType);
+        }
     }
 }
